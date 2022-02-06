ae50d3f1f3bac147ffcaf25a5a2910ce259a3564
Leonel Thiesen <leonelthiesen@gmail.com>
2022-02-05 21:24:48 -0300

    Ajuste conexão banco de dados.

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,11 +22,7 @@ import { SigtapDescricao } from './sigtap/descricao/sigtap-descricao';
         SigtapModule,
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.DB_HOST || 'localhost',
-            port: Number(process.env.DB_PORT || 5432),
-            username: process.env.DB_USERNAME || 'postgres',
-            password: process.env.DB_PASSWORD || 'postgres',
-            database: process.env.DB_DATABASE_NAME || 'tabela-sus',
+            url: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/tabela-sus',
             synchronize: true,
             logging: false,
             entities: [
